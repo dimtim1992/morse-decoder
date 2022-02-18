114b5d0edeb29edae2a7d9f8084343b06cdc9b98
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-02-18 23:19:47 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -37,9 +37,115 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// function decode(expr) {
+//     // write your solution here
+//     let arrDiv = expr.split('');
+//     const arrKeys = Object.keys(MORSE_TABLE);
+//     const arrValues = Object.values(MORSE_TABLE);
+  
+    
+
+//     let arrMorse = arrDiv.map(item => {
+//         if(item === ' ') {
+//            return item = '**********';
+//         } else {
+//            return item = arrKeys[arrValues.findIndex(el => el === item)];
+//         }
+//     });
+  
+  
+
+//     return arrMorse.reduce((sum, item) => {
+//         if(item !== '**********') {
+//             item = item.split('').reduce((sum, item) => {
+//                 if (item === '.') {
+//                     return sum + '10';
+//                 } else {
+//                     return sum + '11';
+//                 }
+//             }, '');
+          
+//             if (item.length < 10) {
+//                 let num = item.length;
+              
+//                 for (let i = 1; i <= 10 - num; i++) {
+//                     item = '0' + item;
+                  
+//                 }
+              
+//             }
+          
+//         } 
+//         return sum + item;
+//     }, '');
+// }
+
 function decode(expr) {
     // write your solution here
-}
+    let arrDiv = expr.split('');
+    let DivLength = arrDiv.length;
+    let compondArr = [];
+    const arrKeys = Object.keys(MORSE_TABLE);
+    const arrValues = Object.values(MORSE_TABLE);
+  
+    
+  
+    while (compondArr.length !== DivLength/10) {
+        compondArr.push(arrDiv.slice(0, 10).join(""));
+        // console.log(compondArr);
+        arrDiv = arrDiv.slice(10); 
+       // console.log(arrDiv);
+    };
+   
+    compondArr = compondArr.map(item => {
+      if (item !== '**********') {
+        return item.slice(item.split('').findIndex(elem => elem > 0));    
+      }
+      return item;
+    });
+  
+    compondArr = compondArr.map(item => {
+      if (item !== '**********') {
+        
+        
+        return item.split('').reduce((sum, it, ind) => {
+            if(ind % 2 === 0) {
+              
+              if(item.slice(ind, ind + 2) === '11') {
+                
+                return sum += '-';
+                
+              } else {
+                
+                return sum += '.';
+               
+              }
+            } else {
+              return sum;    
+            }
+          
+          
+        } ,'');
+      }
+
+      return item;
+    }) 
+  
+  
+  console.log(compondArr);
+  
+  return compondArr.map(item => {
+    if (item === '**********') {
+      return item = ' ';
+    };
+    
+    return item = arrValues[arrKeys.findIndex(el => el === item)];
+  }).join('');
+  
+  
+  
+  
+};
 
 module.exports = {
     decode
